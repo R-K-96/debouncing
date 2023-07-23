7aa0181feedf86cc24b949e91f49cd02d43cf558
R-K-96 <kashmirehrithik@gmail.com>
2023-07-24 00:26:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+  const [showValue, setShowValue] = useState("");
+
+  function showData(e) {
+    let timer = setTimeout(() => {
+      setShowValue(value);
+    }, 500);
+    return () => clearInterval(timer);
+  }
+
+  useEffect(() => {
+    let timer = setTimeout(() => {
+      setShowValue(value);
+    }, 500);
+    return () => clearInterval(timer);
+  }, [value]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Debouncing</h1>
+      <input type="text" onChange={(e) => setValue(e.target.value)}></input>
+      <p>{showValue}</p>
     </div>
   );
 }
